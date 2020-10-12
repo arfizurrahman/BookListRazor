eef50b9fec06f1907b4f26ce49af9694923e4dd6
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-12 18:25:01 +0600

    Implemented delete

--- BookListRazor/BookListRazor/Controllers/BookController.cs
+++ BookListRazor/BookListRazor/Controllers/BookController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BookListRazor.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor.Controllers
 {
@@ -17,9 +18,25 @@ namespace BookListRazor.Controllers
             _db = db;
         }
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new {data = _db.Books.ToList()});
+            return Json(new {data = await _db.Books.ToListAsync()});
         }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Books.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new {success = false, message = "Error while deleting"});
+            }
+
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+
+            return Json(new {success = true, message = "Delete successful"});
+        }
+
     }
 }
---
+++ BookListRazor/BookListRazor/wwwroot/js/bookList.js
@@ -0,0 +1,64 @@
+﻿var dataTable;
+
+$(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_load').dataTable({
+        "ajax": {
+            "url": "/api/book",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "author", "width": "20%" },
+            { "data": "isbn", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/BookList/Edit?id=${data
+                        }" class="btn btn-success text-white" style="curson: pointer; width: 70px;">
+                            Edit
+                        </a>
+                        <a class="btn btn-danger text-white" style="curson: pointer; width: 70px;" onclick=Delete('/api/book?id='+${data})>
+                            Delete
+                        </a>
+                    </div>`;
+                },
+                "width": "40%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No data found"
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: 'Once deleted, you will not be able to recover',
+        icon: 'warning',
+        showCancelButton: true,
+        dangerMode: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                success: function(data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
