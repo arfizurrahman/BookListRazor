e1c393258873430fe634fe2b07d57b8636ba279d
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-12 13:40:36 +0600

    Designed create book page

---
+++ BookListRazor/BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            db = _db;
+        }
+
+        public Book Book { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+}
