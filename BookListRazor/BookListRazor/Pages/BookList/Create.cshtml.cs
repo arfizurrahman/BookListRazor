b3598700d420296cbda05300815496aad803309c
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-12 15:34:41 +0600

    Implemented create

--- BookListRazor/BookListRazor/Pages/BookList/Create.cshtml.cs
+++ BookListRazor/BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -14,12 +14,26 @@ namespace BookListRazor.Pages.BookList
 
         public CreateModel(ApplicationDbContext db)
         {
-            db = _db;
+            _db = db;
         }
 
+        [BindProperty]
         public Book Book { get; set; }
         public void OnGet()
         {
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Books.AddAsync(Book);
+                await _db.SaveChangesAsync();
+                return Redirect("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
